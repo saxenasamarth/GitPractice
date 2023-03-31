3faecb1c838c1d2da7fc238f8bf2a17562d7111e
Samarth <saxenasamarth@gmail.com>
2023-03-31 07:33:30 +0530

    first commit on feature-101

--- Hello.java
+++ Hello.java
@@ -2,7 +2,7 @@ class Hello
 {
 Public static void main(String args[])
 {
-System.out.println(“hello, this is first line”);
+System.out.println(“hello, this is first line of feature-101 java”);
 }
 }
 
